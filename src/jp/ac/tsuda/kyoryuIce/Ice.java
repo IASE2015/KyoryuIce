65d6cf45a3e0d0015b4bc79670d49d7cee786021
g13910ia <g13910ia@gm.tsuda.ac.jp>
2015-05-27 17:21:18 +0900

    できたサーブレットに加えた

--- src/jp/ac/tsuda/kyoryuIce/Ice.java
+++ src/jp/ac/tsuda/kyoryuIce/Ice.java
@@ -2,7 +2,16 @@ package jp.ac.tsuda.kyoryuIce;
 
 import java.io.Serializable;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class Ice implements Serializable{
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
 	private int id;
 	private String name;
 	private int price;
--- src/jp/ac/tsuda/kyoryuIce/KyoryuIceServlet.java
+++ src/jp/ac/tsuda/kyoryuIce/KyoryuIceServlet.java
@@ -1,13 +1,52 @@
 package jp.ac.tsuda.kyoryuIce;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jdo.JDOObjectNotFoundException;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.servlet.ServletContext;
 import javax.servlet.http.*;
+
+import jp.ac.tsuda.kyoryuIce.Ice;
+import jp.ac.tsuda.kyoryuIce.PMF;
 
 @SuppressWarnings("serial")
 public class KyoryuIceServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-	}
+        PersistenceManagerFactory factory = PMF.get();
+        PersistenceManager manager = factory.getPersistenceManager();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        request.setCharacterEncoding("utf-8");
+        String param1 = request.getParameter("id");
+        PrintWriter out = response.getWriter();
+        List<Ice> list = null;
+        if (param1 == null || param1 ==""){
+            String query = "select from " + Ice.class.getName();
+            try {
+                list = (List<Ice>)manager.newQuery(query).execute();
+            } catch(JDOObjectNotFoundException e){}
+        } else {
+            try {
+            	Ice data = (Ice)manager.getObjectById(Ice.class,Long.parseLong(param1));
+                list = new ArrayList();
+                list.add(data);
+            } catch(JDOObjectNotFoundException e){}
+        }
+        String res = "[";
+        if (list != null){
+            for(Ice data:list){
+                res += "{id:" + data.getId() + ",name:'" + data.getName() + "',title:'" +
+                    data.getPrice();
+            }
+        }
+        res += "]";
+        out.println(res);
+        manager.close();
+    }
 }
